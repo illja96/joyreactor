93cfdb6f5f43f7016e742bca9841ed2438bfadc7
Illia Khovanskyi <saskiuhia@gmail.com>
2021-02-11 00:03:28 +0200

    Implement post content insertion

--- src/components/feed/feed-post/feed-post.component.ts
+++ src/components/feed/feed-post/feed-post.component.ts
@@ -1,16 +1,57 @@
-import { Component, Input } from "@angular/core";
+import { Component, OnInit, Input, ViewEncapsulation } from "@angular/core";
+import { JRAttribute } from "../../../models/joy-reactor/attribute.interface";
 import { JRPost } from "../../../models/joy-reactor/post.interface";
 
 @Component({
   selector: 'app-feed-post',
   templateUrl: './feed-post.component.html',
-  styleUrls: ['./feed-post.component.css']
+  styleUrls: ['./feed-post.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class FeedPostComponent {
+export class FeedPostComponent implements OnInit {
   @Input()
   public post: JRPost;
 
+  public bodyHtml: string;
+
   constructor() {
     this.post = undefined!;
+    this.bodyHtml = '';
+  }
+
+  public ngOnInit(): void {
+    if (!this.post) return;
+
+    this.bodyHtml = this.post.text;
+    for (let i = 0; i < this.post.attributes.length; i++) {
+      const attribute = this.post.attributes[i];
+
+      const attributeFileName = this.getAttributeFileName(attribute);
+
+      const attributeHtml = document.createElement('img');
+      attributeHtml.src = `http://img10.joyreactor.cc/pics/post/${attributeFileName}`;
+
+      const replaceTagRegex = `&attribute_insert_${i+1}&`;
+      const isHtmlContainsReplaceTag = this.bodyHtml.match(replaceTagRegex);
+      if (isHtmlContainsReplaceTag) this.bodyHtml = this.bodyHtml.replace(replaceTagRegex, attributeHtml.outerHTML);
+      else this.bodyHtml += attributeHtml.outerHTML;
+    }
+  }
+
+  private getAttributeFileName(attribute: JRAttribute): string {
+    const nameParts = [];
+
+    this.post.blogs
+      .slice(0, 3)
+      .map(b => b.tag)
+      .map(t => t.replace(/[ ./?#]|\s/g, '-'))
+      .forEach(t => nameParts.push(t));
+
+    nameParts.push(attribute.id);
+
+    const name = nameParts.join('-');
+    const nameWithExtension = `${name}.${attribute.image.type}`.toLowerCase();
+
+    return nameWithExtension;
   }
 }
